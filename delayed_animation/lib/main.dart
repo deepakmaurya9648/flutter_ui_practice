6fc018e6f662eca4c29f3775f18fd010bfe6cb9e
DEEPAK MAURYA <dkmaurya9648@gmail.com>
2021-03-16 09:12:55 +0530

    adding ScreenShots in universe app

--- delayed_animation/lib/main.dart
+++ delayed_animation/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:delayed_animation/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animation1/screens/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: HomePAge(),
+      theme: ThemeData.dark(),
+      home: HomePage(),
     );
   }
 }
--- flutter_animation1/lib/screens/homepage.dart
+++ flutter_animation1/lib/screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
+import 'package:flutter_animation1/screens/gesture_animation/gesture_animationScreen.dart';
 
 class HomePAge extends StatefulWidget {
   @override
@@ -29,17 +30,27 @@ class _HomePAgeState extends State<HomePAge>
           return Scaffold(
             body: Transform(
               transform:
-                  Matrix4.translationValues(0, animation.value * width, 0),
+                  Matrix4.translationValues(animation.value * width, 0, 0),
               child: Center(
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.amber,
-                  child: Center(
-                    child: Text(
-                      'Login',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => GestureAnimation()));
+                  },
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.amber,
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
                     ),
                   ),
                 ),
